726e5686c6b24b16c1897d18de7f6db4d7e29f7d
Rick-Rainer Ludwig <ludwig@puresol-technologies.com>
2016-02-07 21:06:40 +0100

    Added DuctileDB twitter timeline.

--- website/app/home.component.ts
+++ website/app/home.component.ts
@@ -1,18 +1,21 @@
 import {Component} from 'angular2/core';
 import {TwitterTimelineComponent} from './twitter-timeline.component';
-import {GooglePlusPostsComponent} from './google-plus-posts.component';
+import {TwitterDuctileDBTimelineComponent} from './twitter-ductiledb-timeline.component';
 
 @Component({
 	selector: 'home',
 	directives: [
 	  TwitterTimelineComponent,
-	  GooglePlusPostsComponent
+	  TwitterDuctileDBTimelineComponent
 	],
 	template: `
-	  <h1>DuctileDB Overview</h1>
-	  <twitter-timeline></twitter-timeline>
-	  <google-plus-posts></google-plus-posts>
-	`
+<h1>DuctileDB Overview</h1>
+<twitter-timeline></twitter-timeline>
+<div>
+  <h1>News</h1>
+  <twitter-ductiledb-timeline></twitter-ductiledb-timeline>
+</div>
+`
 })
 export class HomeComponent {
 }
---
+++ website/app/twitter-ductiledb-timeline.component.ts
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'twitter-ductiledb-timeline',
+	template:
+`<div id="twitter-ductiledb-timeline">
+  <a class="twitter-timeline"  href="https://twitter.com/hashtag/DuctileDB" data-widget-id="696343984327815168">#DuctileDB Tweets</a>
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>`  
+})
+export class TwitterDuctileDBTimelineComponent {
+  ngAfterContentInit(){
+    twttr.widgets.load(document.getElementById("twitter-ductiledb-timeline"));
+  }
+}
